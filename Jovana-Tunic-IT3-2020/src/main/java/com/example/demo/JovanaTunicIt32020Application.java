8507c889263043217dc3a6b5ff324ce5cc7f9a8f
PC <JovanaTunic>
2023-03-31 20:56:12 +0200

    proba da li radi

---
+++ Jovana-Tunic-IT3-2020/src/main/java/com/example/demo/JovanaTunicIt32020Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JovanaTunicIt32020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JovanaTunicIt32020Application.class, args);
+		
+		//proba da li radi 
+	}
+
+}
